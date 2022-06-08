e62ef14b55975b7000252a635278efffa63cf5d4
Isah Ibrahim <isahibn08@gmail.com>
2022-06-08 23:00:00 +0100

    final app creation

--- steps/final-app/src/App.js
+++ steps/final-app/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import { ReactiveBase, SearchBox } from '@appbaseio/reactivesearch';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	var themeURL = window.location.pathname;
+
+	var theme = 'light';
+	if (themeURL === '/') {
+		theme = 'light';
+	} else {
+		theme = themeURL.substring(1);
+	}
+	return (
+		<ReactiveBase
+			app='good-books-ds'
+			url='https://6a0ae3a3a8d4:6a3f508d-169b-4ed7-9680-20658120930f@appbase-demo-ansible-abxiydt-arc.searchbase.io'
+			enableAppbase
+			appbaseConfig={{
+				recordAnalytics: true,
+				userId: 'jon',
+			}}
+			themePreset={theme}
+		>
+			<SearchBox
+				title='SearchBox'
+				defaultValue=''
+				dataField={['original_title', 'original_title.search']}
+				componentId='BookSensor'
+				highlight
+				URLParams
+				enablePopularSuggestions
+				popularSuggestionsConfig={{
+					size: 3,
+					minChars: 2,
+					index: 'good-books-ds',
+				}}
+				enableRecentSuggestions
+				recentSuggestionsConfig={{
+					size: 3,
+					index: 'good-books-ds',
+					minChars: 4,
+				}}
+				size={14}
+				enablePredictiveSuggestions
+				index='good-books-ds'
+				showClear
+				renderNoSuggestion='No suggestions found.'
+				innerClass={{
+					'suggestion-item': 'test-suggestion',
+					'active-suggestion-item': 'active-test-suggestion',
+					'section-header': 'section-header',
+				}}
+				enableFeaturedSuggestions={true}
+				featuredSuggestionsConfig={{
+					featuredSuggestionsGroupId: 'spotlight', // # mandatory
+					maxSuggestionsPerSection: 10,
+					sectionsOrder: ['repositories', 'docs', 'functions'],
+				}}
+			/>
+		</ReactiveBase>
+	);
 }
 
 export default App;
